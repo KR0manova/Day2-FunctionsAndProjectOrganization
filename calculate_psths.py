7c8646aa151a6be1f6b3e604a5ab2e65c027b271
Katerina Romanova <kromanova@tue.mpg.de>
2025-03-06 14:13:04 +0100

    Finished refactoring functions into separate psth_utils.py

--- calculate_psths.py
+++ calculate_psths.py
@@ -1,5 +1,6 @@
 # %% Import library
 from importlib import reload
+import psth_utils
 reload(psth_utils)
 #from psth_utils import download_data
 
@@ -41,66 +42,27 @@ print(spikes)
 # %% Extract Cell-Level Data
 # Exercise: Make an `extract_cells(filename)` function, returning the `cells` variable.
 
-import xarray as xr
-
-def extract_cells(filename):
-    dset = xr.load_dataset(filename)
-    cells = dset['brain_groups'].to_dataframe()
-    return cells
-
-cells=extract_cells(filename)
+reload(psth_utils)
+cells=psth_utils.extract_cells(filename)
 print(cells)
 
 # %% Merge and Compress Extracted Data
 # Exercise: Make a `merge_data(trials, cells, spikes)` function, returning the `merged` variable.
 
-import pandas as pd
-
-def merge_data(trials, cells, spikes):
-    merged = pd.merge(left=cells, left_index=True, right=spikes, right_on='spike_cell')
-    merged = pd.merge(left=trials, right=merged, left_index=True, right_on='spike_trial').reset_index(drop=True)
-    merged.columns
-    merged = (merged
-        .rename(columns=dict(
-            brain_groups="brain_area",
-            spike_trial="trial_id",
-            spike_cell="cell_id",
-            spike_time="time"
-        ))
-        [[
-            'trial_id',
-            'contrast_left',
-            'contrast_right',
-            'stim_onset',
-            'cell_id',
-            'brain_area',
-            'time'
-        ]]
-        .astype(dict(   
-            brain_area = 'category',
-        ))
-        # 
-    )
-    return merged
-
-merged=merge_data(trials, cells, spikes)
+reload(psth_utils)
+merged=psth_utils.merge_data(trials, cells, spikes)
 merged.info()
 
 
 # %% Calculate Time Bins for PSTH
 # Exercise: Make a `compute_time_bins(time, bin_interval)` function, returning the `time_bins` variable.
 
-import numpy as np
+reload(psth_utils)
+
 time = merged['time']
 bin_interval = 0.05
 
-def compute_time_bins(time, bin_interval):
-    time = np.round(time, decimals=6)  # Round time to the nearest microsecond, to reduce floating point errors.
-    time_bins = np.floor(time /bin_interval) * bin_interval  # Round down to the nearest time bin start
-    time_bins
-    return time_bins
-
-time_bins=compute_time_bins(time, bin_interval)
+time_bins=psth_utils.compute_time_bins(time, bin_interval)
 print(time_bins)
 
 # %% filter out stimuli with contrast on the right.
@@ -113,45 +75,19 @@ filtered
 # %% Make PSTHs
 # Exercise: Make a `compute_psths(data, time_bins)` function here, returning the `psth` variable.
 
-psth = (
-    filtered
-    .groupby([time_bins, 'trial_id', 'contrast_left', 'cell_id', 'brain_area'], observed=True, )
-    .size()
-    .rename('spike_count')
-    .reset_index()
-)
-#psth
-psth = (
-    psth
-    .groupby(['time', 'contrast_left', 'brain_area'], observed=True)
-    .spike_count
-    .mean()
-    .rename('avg_spike_count')
-    .reset_index()
-)
-#psth
-
-def compute_psths(data, time_bins):
-    psth['avg_spike_rate'] = psth['avg_spike_count'] * bin_interval
-    return psth
-
-psth=compute_psths(psth, time_bins)
+reload(psth_utils)
+psth=psth_utils.compute_psths(filtered, time_bins, bin_interval)
 psth
 
 
 # %% Plot PSTHs
 # Make a `plot_psths(psth)` function here, returning the `g` variable.
-import seaborn as sns
 
-def plot_psths(psth):
-    g = sns.FacetGrid(data=psth, col='brain_area', col_wrap=2)
-    g.map_dataframe(sns.lineplot, x='time', y='avg_spike_count', hue='contrast_left')
-    g.add_legend()
-    g.savefig('PSTHs.png')
-    return g
+reload(psth_utils)
 
-g=plot_psths(psth)
+g=psth_utils.plot_psths(psth)
 g
+g.savefig('PSTHs.png')
 
 
 # %%
